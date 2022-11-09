706656f88a0c257dd849b84bd33b1e0191b58bf7
Nehalokesh <nehal19697@gmail.com>
2022-11-09 19:40:33 +0530

    task4 commit

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('636b8fa8b6f2b6a771eba13f')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
