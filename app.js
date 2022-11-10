28630c776424deea5c9a3878f0d0f55ddf851754
Nehalokesh <nehal19697@gmail.com>
2022-11-10 14:19:03 +0530

    task9 commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
 
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -19,14 +19,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('636b8fa8b6f2b6a771eba13f')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('636cb210012279ab189b6490')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -39,6 +39,18 @@ app.use(errorController.get404);
 // });
 mongoose.connect('mongodb+srv://Neha:Navya1997@cluster0.61e1t0l.mongodb.net/shop?retryWrites=true&w=majority')
 .then(result => {
+  User.findOne().then(user =>{
+    if(!user){
+      const user = new User({
+        name: 'neha',
+        email: 'neha@gmail.com',
+        cart:{
+          items: []
+        }
+      })
+      user.save();
+    }
+  });
   app.listen(3000);
 })
 .catch(err =>{
--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,4 @@
 
-const product = require('../models/product');
 const Product = require('../models/product');
 
 
@@ -20,7 +19,8 @@ exports.postAddProduct = (req, res, next) => {
     title: title, 
     price:price, 
     description: description,
-    imageUrl:imageUrl
+    imageUrl:imageUrl,
+    userId: req.user
   });
   product
   .save()
@@ -79,7 +79,10 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.find()
+  // .select('title price -_id')  //It will help to select which field should actually retrive from the database
+  // .populate('userId') // It will POP all the user details 
     .then(products => {
+      console.log(products)
       res.render('admin/products', {
         prods: products,
         pageTitle: 'Admin Products',
